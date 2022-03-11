3073466dc9a374b6d5442e201f6595f4239907cc
Stephanie <stephanieanderson5133@gmail.com>
2022-03-11 16:26:15 -0600

    get and posts working within browser

--- Develop/server.js
+++ Develop/server.js
@@ -1,50 +1,60 @@
 const express = require('express');
-//const requestid = require('express-request-id');
+const uniqid = require('uniqid'); 
 const path = require('path');
-const db = require('../db/db.json');
+const db = require('./db/db.json');
 const fs = require('fs');
 const PORT = 3001;
 const app = express();
-const {notesArray} = require('../db/db.json');
+//const {notesArray} = require('./db/db.json');
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //app.use(requestID());
+app.use(express.static('./public'));
 
 //return index file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
   // return notes file
   app.get('/notes', (req, res) => {
-    res.sendFile(__dirname + '/notes.html');
+    res.sendFile(__dirname + '/public/notes.html');
   });
 
+
 // return db file
-app.get('/api/notes', (req, res) => res.json(db));
+app.get('/api/notes', (req, res) => {
+  res.json(db)
+});
 
 // function to add the post to the file
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
+function createNewNote(body, db) {
+    const note = {
+      title: body.title,
+      text: body.text,
+      id: uniqid()
+    };
+    console.log(note)
+    db.push(note);
     fs.writeFileSync(
-      path.join(__dirname, '../db/db.json'),
-      JSON.stringify({ notesArray }, null, 2)
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify(db, null, 2)
     );
-    return notesArray;
+    return db;
   };
 
 // post request to add the post to the file
 app.post('/api/notes', (req, res) => {
-    createNewNote(req.body, notesArray);
-    res.json(db);
+    let x = createNewNote(req.body, db);
+    res.json(x);
+ 
 });
 
 // // trying to add the id, not working
 // app.post('/api/notes', (req, res, next) => {
-//   createNewNote(req.body, notes)
+//   createNewNote(req.body, notesArray)
 //   res.json(db)
 //   res.send(req.id);
 //   next();
@@ -54,3 +64,4 @@ app.post('/api/notes', (req, res) => {
   app.listen(PORT, () => {
     console.log(`app listening at http://localhost:${PORT}`);
   });
+
