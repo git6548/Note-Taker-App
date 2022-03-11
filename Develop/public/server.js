0fbab5a55746cdb0dbd514120cd0302f30e300cc
Stephanie <stephanieanderson5133@gmail.com>
2022-03-11 11:52:00 -0600

    got the requests to work again

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-//const requestid = require('express-request-id')
+//const requestid = require('express-request-id');
 const path = require('path');
 const db = require('../db/db.json');
 const fs = require('fs');
 const PORT = 3001;
 const app = express();
-const {notes} = require('../db/db.json');
+const {notesArray} = require('../db/db.json');
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//app.use(requestID());
 
 //return index file
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
@@ -33,71 +33,24 @@ function createNewNote(body, notesArray) {
       path.join(__dirname, '../db/db.json'),
       JSON.stringify({ notesArray }, null, 2)
     );
-    return note;
-  }
+    return notesArray;
+  };
 
 // post request to add the post to the file
 app.post('/api/notes', (req, res) => {
-    createNewNote(req.body, notes)
-    res.json(db)
-})
+    createNewNote(req.body, notesArray);
+    res.json(db);
+});
 
-// import express from 'express';
-// import requestID from 'express-request-id';
-
-// app.use(requestID());
-
-// app.get('/', function (req, res, next) {
-//     res.send(req.id);
-//     next();
+// // trying to add the id, not working
+// app.post('/api/notes', (req, res, next) => {
+//   createNewNote(req.body, notes)
+//   res.json(db)
+//   res.send(req.id);
+//   next();
 // });
-
-// app.listen(3000, function() {
-//     console.log('Listening on port %d', server.address().port);
-// });
-
-// // curl localhost:3000
-// // d7c32387-3feb-452b-8df1-2d8338b3ea22
-
-
-
-
-
 
   //this connects it to our port
   app.listen(PORT, () => {
     console.log(`app listening at http://localhost:${PORT}`);
   });
-
-
-  //all speculation after this point
-
-//this part is not working
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'notes.html' ));
-//   });
-  
-  //this part is working
-
-  
-  // //this part is working but isn't yet writing to db
-  // app.post('/notes', (req, res) => {
-  //     console.log(">>> incoming post request: ", req.method);
-  //     res.json(req.body);
-  //   })
-  // this part is working
-
-
-
-  
-  
-
-    // app.get('/notes', (req, res) => {
-    //     let results = notes;
-    //   if (req.query) {
-    //       results = createNewNote(req.query, results);
-    //     }
-    //     res.json(results);
-    //   });
-
-    //module.exports = 
